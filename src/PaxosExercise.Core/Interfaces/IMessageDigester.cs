bff27fa5d34cd7031f817fe3bbec50d054de4486
Ferdeen Mughal <ferdeen@gmail.com>
2018-12-09 22:56:41 +0000

    Update IMessageDigester.cs

--- src/PaxosExercise.Core/Interfaces/IMessageDigester.cs
+++ src/PaxosExercise.Core/Interfaces/IMessageDigester.cs
@@ -10,7 +10,6 @@ namespace PaxosExercise.Core.Interfaces
         /// </summary>
         /// <param name="message"> Message text that's being converted into a hash.</param>
         /// <param name="cancellationToken"><see cref="CancellationToken"/> Notify a cancellation of the task.</param>
-        /// <returns>A <see cref="GetColdStakingInfoResponse"/> object containing the cold staking information.</returns>
         Task<string> ComputeMessageDigestAsync(string message, CancellationToken cancellationToken = default(CancellationToken));
     }
 }
